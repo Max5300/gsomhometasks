91f6a083916d95145e1803e80bd4a9fd3c4f6e64
Max5300 <47719541+Max5300@users.noreply.github.com>
2019-02-18 00:17:28 +0300

    Create table.py

---
+++ table.py
@@ -0,0 +1,11 @@
+n = int(input())
+if n == 3:
+    print("Li")
+elif n == 25:
+    print("Mn")
+elif n == 80:
+    print("Hg")
+elif n == 17:
+    print("Cl")
+else:
+    print("Пожалуйста, введите допустимый номер элемента")
